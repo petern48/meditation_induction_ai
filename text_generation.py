dccce0a6e6e4761d13e176952db3c172c2be4294
Peter Nguyen <petern0408@gmail.com>
2023-09-19 19:24:26 -0700

    Update text generation prompts

--- text_generation.py
+++ text_generation.py
@@ -19,8 +19,8 @@ def text_generation(selected_type):
 
     prompts = {
         'focused': 'write me a focused meditation script designed to enhance focus and attention by noticing all 5 senses',
-        'body-scan': 'write me a body scan meditation script to relax and relieve stress by tightening and relaxing muscles',
-        'visualization': 'write me a visualization meditation script noticing all 5 senses at the beach/garden and is designed to boost mood, reduce stress, and promote inner peace',
+        'body-scan': 'write me a body scan meditation script designed to relax and relieve stress by tightening and relaxing muscles',
+        'visualization': 'write me a visualization meditation script designed to boost mood, reduce stress, and promote inner peace by noticing all 5 senses at the beach',  # or garden
         'reflection': 'write me a reflection meditation script designed to increase self awareness, mindfulness, and gratitude by asking the user about the current day and the recent past',
         'movement': 'write me a movement meditation script designed to improve mind body connection, energy, vitality, and the systems of the body'
     }
