9d68b40d5ca0b3ca6f5b0bdeaa7291f901f07d86
Peter Nguyen <petern0408@gmail.com>
2023-09-26 23:08:07 -0700

    Add sentiment analysis to text_to_audio.py. Now return list of sentiments, and list of audio segments.

--- src/text_to_audio.py
+++ src/text_to_audio.py
@@ -3,41 +3,49 @@ import math
 import os
 from pydub import AudioSegment
 import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 
 def text_to_speech(meditation_script, accent, filename):
     """Generates audio file given using gTTS library"""
-    # gTTS implementation
 
     # List of accents available in gTTS
     if accent not in ['com.au', 'co.uk', 'us', 'ca', 'co.in', 'ie', 'co.za']:
-        raise Exception('Incorrect accent')
+        raise Exception('Invalid accent')
 
-    # segments = meditation_script.split('. ')
-    segments = nltk.sent_tokenize(meditation_script)
+    sentiments = []
+    audio_segments = []  # Not in original program
 
+    sentences = nltk.sent_tokenize(meditation_script)
 
     # Add longer pause after every sentence
     pause = AudioSegment.silent(duration=2000)  # milliseconds
     speech_audio = AudioSegment.empty()
-    for segment in segments:
-        # Create the TTS object
+    analyzer = SentimentIntensityAnalyzer()
+
+    for sentence in sentences:
+        # Text to speech
         tts = gTTS(
-            segment,
+            sentence,
             tld=accent,    # Indian voice sounded the most soothing
             lang="en",
             slow=True
         )
         tts.save('segment.mp3')
-        segment_audio = AudioSegment.from_mp3('segment.mp3')
-        speech_audio += segment_audio + pause
+        segment_audio = AudioSegment.from_mp3('segment.mp3') + pause
+        audio_segments.append(segment_audio)
+        speech_audio += segment_audio
+
+        # Sentiment Analysis
+        sentiment_score = analyzer.polarity_scores(sentence)['compound']
+        sentiments.append(sentiment_score)
 
     # Save the audio file
     print(f"Saving {filename} audio file")
     speech_audio.export(filename, format='mp3')
     os.remove('segment.mp3')  # Remove the temporary file
 
-    return filename
+    return filename, audio_segments, sentiments, sentences
 
 
 def overlay_music_and_speech(speech_file_path, music_file_path, filename):
@@ -57,3 +65,16 @@ def overlay_music_and_speech(speech_file_path, music_file_path, filename):
     combined.export(filename, format="mp3")
 
     return filename
+
+# For testing purposes
+if __name__ == '__main__':
+    filename = 'examples/llama2-chat-body-scan.txt'
+    with open(filename, 'r') as f:
+        script = f.read()
+    filename, audio_segments, sentiments, sentences = text_to_speech(script, 'co.in', 'temp_output.mp3')
+    print(filename)
+    for i in range(len(sentences)):
+        print(audio_segments[i])
+        print(sentences[i])
+        print(sentiments[i])
+        print()
